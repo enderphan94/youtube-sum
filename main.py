3fd3ec9523a778913d273bd5c647d92a4a4f5004
enderphan94 <lvan@rnglabs.net>
2024-07-27 21:25:02 +0800

    first add

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import os
 
 app = Flask(__name__)
 
-#openai.api_key = 'sk-proj-87PFObUVCE8cahOVGMSaT3BlbkFJYHdxnzLbAg8xAVVeo4Xc'
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
 @app.route('/', methods=['GET', 'POST'])
